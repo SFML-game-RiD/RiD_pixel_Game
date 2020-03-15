177fb1908fdae7d9b9852ca7091d300eea284d98
Michał <34627723+michal1702@users.noreply.github.com>
2020-03-15 12:59:48 +0100

    Engine - animation

--- RiD_RPGproject/RiD_RPGproject/Engine/Animation.cpp
+++ RiD_RPGproject/RiD_RPGproject/Engine/Animation.cpp
@@ -4,19 +4,27 @@
 
 namespace RiD
 {
-	Animation::Animation(sf::IntRect start_rectangle, double width, double end, sf::Time frame_duration, sf::Texture texture) :
-		_frame_start(sf::Time::Zero), _width(width), _end(end), _frame_duration(frame_duration), _rectangle(start_rectangle), _texture(texture)
+	Animation::Animation(sf::IntRect start_rectangle, short width, short number_of_frames, sf::Time frame_duration, sf::Texture texture, short playback_mode) :
+		_frame_start(sf::Time::Zero), _width(width), _number_of_frames(number_of_frames), _frame_duration(frame_duration), _rectangle(start_rectangle), _texture(texture), _playback_mode(playback_mode), _is_triggered(false)
 	{
 		_object.setTexture(_texture);
 		_object.setTextureRect(_rectangle);
+		_end = (_number_of_frames-1) * width;
 	}
 
 
 	void Animation::updateAnimation(sf::Time time)
 	{
-		if (time.asSeconds() - _frame_start.asSeconds() >= _frame_duration.asSeconds()) {
-			if (_rectangle.left == _end)
+		if (time.asSeconds() - _frame_start.asSeconds() >= _frame_duration.asSeconds() && _is_triggered == true) {
+			if (_rectangle.left == _end && _playback_mode == loop)
 				_rectangle.left = 0;
+			else if (_rectangle.left == _end && _playback_mode == single)
+			{
+				_rectangle.left = 0;
+				_object.setTextureRect(_rectangle);
+				_is_triggered = false;
+				return;
+			}
 			else
 				_rectangle.left += _width;
 
@@ -28,6 +36,18 @@ namespace RiD
 	sf::Sprite Animation::getObjectSprite()
 	{
 		return this->_object;
+	}
+
+	void Animation::triggerAnimation()
+	{
+		_is_triggered = true;
+	}
+
+	void Animation::stopAnimation()
+	{
+		_rectangle.left = 0;
+		_object.setTextureRect(_rectangle);
+		_is_triggered = false;
 	}
 
 }
--- RiD_RPGproject/RiD_RPGproject/Engine/Animation.h
+++ RiD_RPGproject/RiD_RPGproject/Engine/Animation.h
@@ -8,19 +8,23 @@ namespace RiD
 	class Animation
 	{
 	private:
-		double _width, _end;
+		bool _is_triggered, _is_playing;
+		short _number_of_frames, _playback_mode, _width, _end;
 		sf::Time _frame_start, _frame_duration;
 		sf::Sprite _object;
 		sf::IntRect _rectangle;
 		sf::Texture _texture;
 	public:
+		enum playbackMode { loop, single };
 		/*
 		@param start_rectangle starting rect
 		@param width width of the frame
-		@param end end of the frame
+		@param number_of_frames 
 		@param frame_duration frame duration
+		@param texture
+		@param playback_mode loop or single
 		*/
-		Animation(sf::IntRect start_rectangle, double width, double end, sf::Time frame_duration, sf::Texture texture);
+		Animation(sf::IntRect start_rectangle, short width, short number_of_frames, sf::Time frame_duration, sf::Texture texture, short playback_mode);
 
 		/*
 		Updates animation
@@ -30,5 +34,11 @@ namespace RiD
 
 		//Returns sprite ready to be drawn
 		sf::Sprite getObjectSprite();
+
+		//Starts animation
+		void triggerAnimation();
+
+		//Stops animation
+		void stopAnimation();
 	};
 }
