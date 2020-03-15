a46c54ccc9c24010ef40c72e07dfd23005d313f4
Michał <34627723+michal1702@users.noreply.github.com>
2020-03-15 16:22:56 +0100

    idle and walking animation

---
+++ RiD_RPGproject/RiD_RPGproject/Engine/Movement.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "AssetManager.h"
+
+namespace RiD
+{
+	class Movement
+	{
+	private:
+		short _row=0, _xCord=0, _yCord=0, _direction=0;
+		enum directions { up, left, down, right };
+		sf::Texture _texture;
+		sf::Sprite _object;
+		sf::Time _animation_start_time;
+		sf::Time _animation_frame_duration;
+	public:
+		Movement(sf::Texture texture);
+
+		/*
+		Walking animation with movement included
+		@param starting_row row in *.png file where desired frames starts
+		@param time
+		*/
+		void walking(short starting_row, sf::Time time);
+
+		/*
+		Idle animation
+		@param time
+		*/
+		void idle(sf::Time time);
+
+		//Returns sprite
+		sf::Sprite getSprite();
+	};
+}
