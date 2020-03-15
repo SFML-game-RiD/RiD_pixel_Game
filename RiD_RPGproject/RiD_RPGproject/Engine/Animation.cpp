f6fc6162009319114f31091ae423d3b7e394e9b7
Michał <34627723+michal1702@users.noreply.github.com>
2020-03-15 16:23:33 +0100

    Methods added to animation class

--- RiD_RPGproject/RiD_RPGproject/Engine/Animation.cpp
+++ RiD_RPGproject/RiD_RPGproject/Engine/Animation.cpp
@@ -12,6 +12,9 @@ namespace RiD
 		_end = (_number_of_frames-1) * width;
 	}
 
+	Animation::Animation():_is_triggered(false), _frame_start(sf::Time::Zero)
+	{
+	}
 
 	void Animation::updateAnimation(sf::Time time)
 	{
@@ -48,6 +51,29 @@ namespace RiD
 		_rectangle.left = 0;
 		_object.setTextureRect(_rectangle);
 		_is_triggered = false;
+	}
+
+	void Animation::setParameters(sf::IntRect start_rectangle, short width, short number_of_frames, sf::Time frame_duration, sf::Texture texture, short playback_mode)
+	{
+		_rectangle = start_rectangle;
+		_object.setTextureRect(_rectangle);
+		_width = width;
+		_number_of_frames = number_of_frames;
+		_end = (_number_of_frames - 1) * width;
+		_frame_duration = frame_duration;
+		_playback_mode = playback_mode;
+		_texture = texture;
+		_object.setTexture(_texture);
+	}
+
+	void Animation::setSpritePosition(sf::Vector2f position)
+	{
+		_object.setPosition(position.x, position.y);
+	}
+
+	void Animation::moveSprite(sf::Vector2f new_position)
+	{
+		_object.move(new_position.x, new_position.y);
 	}
 
 }
